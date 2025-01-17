865351cffa5d10ef9e380da30f675b6a6188ec33
smalloir <mallsam611@gmail.com>
2025-01-17 21:44:17 +0100

    visualizer reading instructions with getline

--- visualizer/main.cpp
+++ visualizer/main.cpp
@@ -8,12 +8,14 @@ int main(int ac, char **av)
     if (ac == 1)
         return (1);
 
-    //  initialize puzzle class
+    // initialize puzzle class
     Npuzzle npuzzle;
 
+    // parse puzzle file and get map data
     if (npuzzle_parsing(av, npuzzle))
         return (1);
 
+    // store npuzzle map data string in data vector
     std::vector<std::string> data;
     for (unsigned int i = 0; i < npuzzle._map.size(); i++)
     {
@@ -23,10 +25,20 @@ int main(int ac, char **av)
         }
     }
 
+    // get instructions as argv
     std::string tmp, instructions;
     for (int i = 2; i < ac; i++)
         tmp += av[i];
 
+    // get instructions with getline
+    if (tmp.empty())
+    {
+        std::string input;
+        while (std::getline(std::cin, input))
+            tmp += input;
+    }
+
+    // parse instructions
     for (std::size_t i = 0; i + 2 < tmp.size(); i++)
     {
         if (int(tmp[i]) == -30 && int(tmp[i + 1]) == -122)
@@ -51,6 +63,7 @@ int main(int ac, char **av)
         }
     }
 
+    // run visualizer
     Visualizer visualizer(1300, 900, "N-puzzle", npuzzle.get_size());
     visualizer.copy_vector(data);
     visualizer.set_instructions(instructions);
